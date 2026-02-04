e7ba0eef4c8619f6bc0a3bba68dd599494c4bcd5
HoWilgh <wilfried.hortschitz@donau-uni.ac.at>
2026-02-04 08:28:49 +0100

    Fix file name inconsistencies in create.sh for ontology and documentation

--- create.sh
+++ create.sh
@@ -6,14 +6,13 @@ CHECK=emmocheck
 
 echo "remove previous files ..."
 rm -f magneticmaterials.sqlite3
-rm -f magnetic_material_mammos.ttl
+rm -f magnetic_materials_ontology_mammos.ttl
 
 echo "building ontology ..."
 python src/build_onto.py
-ls -ls magnetic_material_mammos.ttl
-
+ls -ls magnetic_materials_ontology_mammos.ttl
 echo "checking ontology ..."
-$CHECK magnetic_material_mammos.ttl
+$CHECK magnetic_materials_ontology_mammos.ttl
 
 
 # on ARM Mac, this seems required once to build documentation
@@ -21,6 +20,6 @@ doc -c
 
 echo "building documentation ..."
 cd doc
-python ./mammosdoc --template=mammos.md --local --format=html ../magnetic_material_mammos.ttl magnetic_material_mammos.html
+python ./mammosdoc --template=mammos.md --local --format=html ../magnetic_materials_ontology_mammos.ttl magnetic_materials_ontology_mammos.html
 cd ..
-ls -ls doc/magnetic_material_mammos.html
+ls -ls doc/magnetic_materials_ontology_mammos.html
