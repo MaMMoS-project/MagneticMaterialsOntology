77f64f1e27bee700e9f592ff730ee0daa959b088
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-02-06 10:18:10 +0100

    add saturationmagneticpolarization

--- src/build_onto.py
+++ src/build_onto.py
@@ -954,6 +954,44 @@ with onto:
         is_a = [emmo.hasMeasurementUnit.some(EnergyDensityUnit)]
         wikipediaReference = pl("https://en.wikipedia.org/wiki/Maximum_energy_product")
 
+    class SaturationMagneticPolarization(emmo.ElectromagneticQuantity):
+        """The Saturation magnetic polarization Jsat is the maximum
+        obtainable magnetic polarization for a given substance
+        at a given temperature. Jsat should be used instead of Js to avoid
+        confusion with the symbol for the spontaneous polarization"""
+
+        prefLabel = en("SaturationMagneticPolarization")
+        altLabel = [
+            enUS("SaturationMagneticPolarization"),
+            enGB("SaturationMagneticPolarisation"),
+            en("Jsat"),
+            en("Js"),
+        ]
+        is_a = [emmo.hasMeasurementUnit.some(emmo.MagneticFluxDensityUnit)]
+        IECEntry = pl(
+            "https://www.electropedia.org/iev/iev.nsf/display?openform&ievref=221-01-05"
+        )
+
+    class SaturationMagnetization(emmo.ElectromagneticQuantity):
+        """The Saturation magnetization Msat is the maximum
+        obtainable magnetic magnetization for a given substance
+        at a given temperature. Msat should be used instead Ms to avoid
+        confusion with the symbol for the SpontaneousMagnetization"""
+
+        prefLabel = en("SaturationMagnetization")
+        altLabel = [
+            enUS("SaturationMagnetization"),
+            enGB("SaturationMagnetisation"),
+            en("Msat"),
+            en("Ms"),
+        ]
+        is_a = [emmo.hasMeasurementUnit.some(emmo.MagneticFieldStrengthUnit)]
+        IECEntry = pl(
+            "https://www.electropedia.org/iev/iev.nsf/display?openform&ievref=221-01-04"
+        )
+        wikipediaReference = pl("https://en.wikipedia.org/wiki/Saturation_(magnetic)")
+        wikidataReference = pl("https://www.wikidata.org/wiki/Q2630994")
+
     # -----------------------------------------------------
 
     class MagneticHysteresisProperties(emmo.Property):
@@ -986,6 +1024,7 @@ with onto:
             emmo.hasProperty.min(0, KneeFieldExternal),
             emmo.hasProperty.exactly(1, Remanence),
             emmo.hasProperty.min(0, RemanentMagneticPolarization),
+            emmo.hasProperty.min(0, SaturationMagneticPolarization),
             emmo.hasProperty.min(0, MaximumEnergyProduct),
         ]
 
